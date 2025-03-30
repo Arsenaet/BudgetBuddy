7eeac5d335ff227f3eb145819b3fc61ff6e43345
Brownson Omotoye <omotoyeb@sheridancollege.ca>
2025-03-29 22:04:57 -0400

    created the tailwind file i will use as the template for the dark mode

---
+++ static/scripts/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    darkMode: 'class',
+    theme: {
+      extend: {
+        colors: {
+          dark: {
+            800: '#1e293b',
+            900: '#0f172a',
+          }
+        }
+      }
+    }
+  }
